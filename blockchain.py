dfb43b0e7fa7110412f402312a0e5f3215a865a4
mehdi5599 <82046514+mehdi5599@users.noreply.github.com>
2023-02-15 03:14:13 +0330

    Revert "Accept URLs without scheme (also includes #18 and #28)"

--- blockchain.py
+++ blockchain.py
@@ -25,14 +25,7 @@ class Blockchain:
         """
 
         parsed_url = urlparse(address)
-        if parsed_url.netloc:
-            self.nodes.add(parsed_url.netloc)
-        elif parsed_url.path:
-            # Accepts an URL without scheme like '192.168.0.5:5000'.
-            self.nodes.add(parsed_url.path)
-        else:
-            raise ValueError('Invalid URL')
-
+        self.nodes.add(parsed_url.netloc)
 
     def valid_chain(self, chain):
         """
